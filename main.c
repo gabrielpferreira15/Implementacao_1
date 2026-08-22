33633839b9fc7e1c45651f427463c871a7ec4a60
gabrielpferreira15 <gpsf@cesar.school>
2026-08-22 18:00:19 -0300

    Primeira versão da captura de tasks

--- main.c
+++ main.c
@@ -2,7 +2,17 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct task{
+    char *nome;
+    char *cmd;
+    char *args[50];
+} task;
+
 int main(){
+    task catalogo[100];
+    int tarefas_totais = 0;
+    int indice = 0;
+
     while (1){
         char entrada[1024];
 
@@ -24,16 +34,31 @@ int main(){
             break;
         }
 
-        printf("Comando detectado: %s\n", comando);
+        if((strcmp(comando, "task") == 0)){ 
 
-        char *argumento = strtok(NULL, " ");
+            char *argumento = strtok(NULL, " ");
 
-        while (argumento != NULL) {
-
-            printf("Comando detectado: %s\n", argumento);
+            catalogo[tarefas_totais].nome = strdup(argumento);
 
             argumento = strtok(NULL, " ");
 
+            catalogo[tarefas_totais].cmd = strdup(argumento);
+
+            argumento = strtok(NULL, " ");
+
+            catalogo[tarefas_totais].args[indice] = strdup(argumento);
+            indice++;
+
+            argumento = strtok(NULL, " ");
+
+            while(argumento != NULL){
+                catalogo[tarefas_totais].args[indice] = strdup(argumento);
+
+                argumento = strtok(NULL, " ");
+                indice++;
+            }
+            indice = 0;
+            tarefas_totais++;
         }
     }
 
