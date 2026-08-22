b8e1f88169ef1c01ffc1c9ca1997e2afb634b562
gabrielpferreira15 <gpsf@cesar.school>
2026-08-22 18:55:49 -0300

    Ajuste e polimento do armazenamento de tasks

--- main.c
+++ main.c
@@ -11,7 +11,6 @@ typedef struct task{
 int main(){
     task catalogo[100];
     int tarefas_totais = 0;
-    int indice = 0;
 
     while (1){
         char entrada[1024];
@@ -36,28 +35,40 @@ int main(){
 
         if((strcmp(comando, "task") == 0)){ 
 
+            if (tarefas_totais >= 100) {
+            printf("Erro: Limite de tarefas atingido.\n");
+            continue;
+            }
+
+            char *token_nome = strtok(NULL, " ");
+            if (token_nome == NULL) {
+                printf("Erro: Faltando nome da tarefa.\n");
+                continue;
+            }
+
+            catalogo[tarefas_totais].nome = strdup(token_nome);
+
+            char *token_cmd = strtok(NULL, " ");
+            if (token_cmd == NULL) {
+                printf("Erro: Faltando programa da tarefa.\n");
+                continue;
+            }
+
+            catalogo[tarefas_totais].nome = strdup(token_cmd);
+
+            catalogo[tarefas_totais].args[0] = strdup(token_cmd);
+
+            int indice = 1;
             char *argumento = strtok(NULL, " ");
 
-            catalogo[tarefas_totais].nome = strdup(argumento);
-
-            argumento = strtok(NULL, " ");
-
-            catalogo[tarefas_totais].cmd = strdup(argumento);
-
-            argumento = strtok(NULL, " ");
-
-            catalogo[tarefas_totais].args[indice] = strdup(argumento);
-            indice++;
-
-            argumento = strtok(NULL, " ");
-
-            while(argumento != NULL){
+            while(argumento != NULL && indice < 49){
                 catalogo[tarefas_totais].args[indice] = strdup(argumento);
-
-                argumento = strtok(NULL, " ");
                 indice++;
+                argumento = strtok(NULL, " ");
             }
-            indice = 0;
+
+            catalogo[tarefas_totais].args[indice] = NULL;
+
             tarefas_totais++;
         }
     }
