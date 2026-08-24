2d43fffefc392a501f25f8510ca92dd05bd0dc96
gabrielpferreira15 <gpsf@cesar.school>
2026-08-23 23:30:27 -0300

    Ajuste final do pipe

--- task.c
+++ task.c
@@ -118,11 +118,12 @@ void executar_pipe(task *tasks[], int total) {
     for (int i = 0; i < total; i++) {
         int fd_atual[2];
 
-        if (pipe(fd_atual) < 0) {
-            perror("Erro ao criar pipe");
-            return;
+        if (i < total - 1) {
+            if (pipe(fd_atual) < 0) {
+                perror("Erro ao criar pipe");
+                return;
+            }
         }
-
         pids[i] = fork();
 
         if (pids[i] == 0) {
